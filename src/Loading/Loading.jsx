1583f3c4ed9bb28cb852be9685eef9408c05f0f9
Sergen Skenderi <sergen@publer.io>
2023-11-27 00:14:56 +0100

    feat: created loading component

---
+++ src/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './loading.css';
+
+const Loading = () => {
+  return (
+    <div className="loading-dots">
+      <div className="loading-dot"></div>
+      <div className="loading-dot"></div>
+      <div className="loading-dot"></div>
+    </div>
+  );
+};
+
+export default Loading;

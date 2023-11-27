389d6611bcb4ffb07ea70a73ef83e5a5bcafb92d
Sergen Skenderi <sergen@publer.io>
2023-11-27 10:25:19 +0100

    feat: handle pagination, fetch data on page change

--- src/Characters/CharactersPage.jsx
+++ src/Characters/CharactersPage.jsx
@@ -5,16 +5,18 @@ import CharactersCard from "./CharacterCard";
 import "./characters.css";
 
 const CharactersPage = (props) => {
-    const { characters, error, currentPage, handlePageChange, loading } = props;
+    const { characters, error, currentPage, handlePageChange, loading, totalPages } = props;
     
     return (
         error ? <div className="error-container">Something went wrong. Please refresh the page or contact with support.</div> :
-         loading ? <div className="loading-container"><Loading /></div> : <div className="characters-container">
+          <div className="characters-container">
             <Filter />
+            {loading ? <div className="loading-container"><Loading /></div> : characters && characters.length ? <>
             <div className="characters-grid-container">
                 {characters.map((character,index) => <CharactersCard key={index} name={character.name} />)}
             </div>
-            <Pagination totalPages={10} onPageChange={handlePageChange} currentPage={currentPage} />
+            <Pagination totalPages={totalPages} onPageChange={handlePageChange} currentPage={currentPage} />
+            </> : <div className="error-container">No results found</div>}
         </div>
     )
 }
